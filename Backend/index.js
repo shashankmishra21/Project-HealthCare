d0f06a46a45ceb8259c44887ab097cb8e4c2add8
shashankmishra21 <mishrashashank2106@gmail.com>
2025-06-17 17:08:57 +0530

    database connected

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./config/db");
+require("dotenv").config();
 const PORT = 3000;
+
+
+// Connect to MongoDB
+connectDB();
 
 // Middleware
 app.use(express.json());
@@ -13,4 +19,6 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
+    
 });
+
